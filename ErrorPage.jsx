2891247ca4575b3e01959dfcd7987f61e5c39596
monisree81 <167197671+monisree81@users.noreply.github.com>
2025-10-31 12:19:49 +0530

    Add ErrorPage component for error handling

---
+++ ErrorPage.jsx
@@ -0,0 +1,13 @@
+// src/Components/ErrorPage.jsx
+import React from 'react';
+
+const ErrorPage = () => {
+  return (
+    <div>
+      <h1>Something Went Wrong</h1>
+      <p>We're sorry, but an error occurred. Please try again later.</p>
+    </div>
+  );
+};
+
+export default ErrorPage;
