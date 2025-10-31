b8447dfbc01c27f83697af5dbbd5de8b7fbc071c
monisree81 <167197671+monisree81@users.noreply.github.com>
2025-10-31 12:36:16 +0530

    Refactor ErrorPage component to use implicit return

--- ErrorPage.jsx
+++ ErrorPage.jsx
@@ -1,13 +1,10 @@
-// src/Components/ErrorPage.jsx
 import React from 'react';
 
-const ErrorPage = () => {
-  return (
-    <div>
-      <h1>Something Went Wrong</h1>
-      <p>We're sorry, but an error occurred. Please try again later.</p>
-    </div>
-  );
-};
+const ErrorPage = () => (
+  <div>
+    <h1>Something Went Wrong</h1>
+    <p>We're sorry, but an error occurred. Please try again later.</p>
+  </div>
+);
 
 export default ErrorPage;
