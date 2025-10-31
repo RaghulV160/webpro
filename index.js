7b43b725202fc79793a11666a4632c31a41d00a0
monisree81 <167197671+monisree81@users.noreply.github.com>
2025-10-31 12:19:03 +0530

    Initialize Redux store with root reducer

---
+++ index.js
@@ -0,0 +1,15 @@
+// src/store/index.js
+import { createStore } from 'redux';
+
+const initialState = {};
+
+function rootReducer(state = initialState, action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
+
+const store = createStore(rootReducer);
+
+export default store;
