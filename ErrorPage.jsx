dead647a15be66dca05b04507b8a9bf7dcf657b6
monisree81 <167197671+monisree81@users.noreply.github.com>
2025-10-31 12:53:20 +0530

    Refactor ErrorPage component to use return statement

--- ErrorPage.jsx
+++ ErrorPage.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-
-const ErrorPage = () => (
-  <div>
-    <h1>Something Went Wrong</h1>
-    <p>We're sorry, but an error occurred. Please try again later.</p>
-  </div>
+const ErrorPage = () => {
+return (
+<div>
+<h1>Something Went Wrong</h1>
+<p>We're sorry, but an error occurred. Please try again later.</p>
+</div>
 );
-
+};
 export default ErrorPage;
